871586a40e6934eddb3e81b28e20988eb09d516e
付武 <wu.fu@shengydt.com>
2023-12-01 17:10:27 +0800

    数据云存储、身份登录验证测试添加

--- Assets/Scripts/UnityServicesDemo.cs
+++ Assets/Scripts/UnityServicesDemo.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Unity.Services.Authentication;
+using Unity.Services.CloudCode;
+using Unity.Services.CloudSave;
 using Unity.Services.Core;
 using UnityEngine;
 
@@ -21,14 +24,24 @@ public class UnityServicesDemo : MonoBehaviour
             Debug.LogException(e);
         }
 
-        //身份验证登录
+        //身份验证匿名异步登录
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+        /*try
+        {
+            string result = await CloudCodeService.Instance.CallEndpointAsync("ExampleModule",  new Dictionary<string, object> { {"name"," World"} });
+        }
+        catch(CloudCodeException exception)
+        {
+            Debug.LogException(exception);
+        }*/
     }
 
     private void Start()
     {
         AddSignCallBack();
     }
+
 
     /// <summary>
     /// 添加登录响应事件
@@ -69,5 +82,54 @@ public class UnityServicesDemo : MonoBehaviour
         Debug.Log($"Is SignedIn: {AuthenticationService.Instance.IsSignedIn}");
         Debug.Log($"Is Authorized: {AuthenticationService.Instance.IsAuthorized}");
         Debug.Log($"Is Expired: {AuthenticationService.Instance.IsExpired}");
+        Debug.Log("A");
+        SaveDataExample();
+        Debug.Log("B");
+        LoadDataExample();
+        Debug.Log("C");
+        RetrieveKeys();
+        Debug.Log("D");
+
     }
+
+    /// <summary>
+    /// 保存游戏数据示例
+    /// </summary>
+    private async void SaveDataExample()
+    {
+        var data = new Dictionary<string, object> { { "key", "someValue" },{ "key_01","test_value"} };
+        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
+    }
+    /// <summary>
+    /// 加载游戏数据示例
+    /// </summary>
+    private async void LoadDataExample()
+    {
+        Dictionary<string, string> savedData = await CloudSaveService.Instance.Data.LoadAsync(new HashSet<string> { "key" });
+        Debug.Log("Done: " + savedData["key"]);
+    }
+    /// <summary>
+    /// 删除数据
+    /// </summary>
+    private async void DeleteDataExample()
+    {
+        await CloudSaveService.Instance.Data.ForceDeleteAsync("key");
+    }
+    /// <summary>
+    /// 获取密钥和元数据的列表
+    /// </summary>
+    private async void RetrieveKeys()
+    {
+        List<string> keys = await CloudSaveService.Instance.Data.RetrieveAllKeysAsync();
+
+        foreach(string key in keys)
+        {
+            Debug.Log("key----:"+ key);
+        }
+    }
+
+    /*public async void GetPlayerFileAsByteArray()
+    {
+        byte[] file = await CloudSaveService.Instance.Files.Player.LoadBytesAsync("fileName.csv");
+    }*/
 }
